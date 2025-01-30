3bae6935270a62383df4225ea51817ae95fd6006
EGDarko <96339057+EGDarko@users.noreply.github.com>
2025-01-30 23:15:33 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import requests
 from colorama import Fore, Style, init
 from concurrent.futures import ThreadPoolExecutor
-
+import time
 
 init(autoreset=True)
 
@@ -38,14 +38,14 @@ def check_subdomain(url, output_file):
 
 
 def main():
-   
+     
     try:
         num_threads = int(input("Enter the number of threads (default 10): "))
     except ValueError:
         num_threads = 10 
 
     print(f"Using {num_threads} threads for parallel processing...\n")
-
+    start_time = time.time()
     print(f"""
 {Fore.GREEN}    .▄▄ · ▄• ▄▌▄▄▄▄·  ▄▄·  ▄ .▄▄ •▄ {Style.RESET_ALL}
 {Fore.GREEN}    ▐█ ▀. █▪██▌▐█ ▀█▪▐█ ▌▪██▪▐██▌▄▌▪{Style.RESET_ALL}
@@ -58,7 +58,7 @@ def main():
     print(f"{Fore.RED}!notice You should put the subdomains list in : subdomains.txt{Style.RESET_ALL}")
     print("Tool started.... ")
 
-    # Open the output file to write results
+     
     with open("output_results.txt", "w") as output_file:
         with open("subdomains.txt", "r") as subs:
            
@@ -67,8 +67,9 @@ def main():
             with ThreadPoolExecutor(max_workers=num_threads) as executor: 
                 
                 executor.map(lambda url: check_subdomain(url, output_file), subdomains)
-
+    end_time = time.time()
+    elapsed_time = end_time - start_time  
     print("Scan Finished. Results saved to 'output_results.txt'")
-
+    print(f"Execution Time: {elapsed_time:.2f} seconds")
 
 main()
