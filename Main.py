a0028d7b95d56cbd82597b27451d41b1339f7f32
EGDarko <96339057+DARKO0001@users.noreply.github.com>
2025-01-30 17:00:41 +0200

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,74 @@
+import requests
+from colorama import Fore, Style, init
+from concurrent.futures import ThreadPoolExecutor
+
+
+init(autoreset=True)
+
+
+def format_url(url):
+    if not url.startswith(("http://", "https://")):
+        return "http://" + url
+    return url
+
+
+def check_subdomain(url, output_file):
+    url = format_url(url)
+    try:
+        response = requests.get(url, timeout=5)
+        status_code = response.status_code
+    except requests.RequestException:
+        status_code = "Error"
+
+    
+    if status_code == 200:
+        color = Fore.GREEN
+    elif status_code == 404:
+        color = Fore.RED
+    elif status_code == 403:
+        color = Fore.YELLOW
+    else:
+        color = Fore.CYAN
+
+    
+    print(f"{color}{status_code}{Style.RESET_ALL} - {url}")
+
+   
+    output_file.write(f"{status_code} - {url}\n")
+
+
+def main():
+   
+    try:
+        num_threads = int(input("Enter the number of threads (default 10): "))
+    except ValueError:
+        num_threads = 10 
+
+    print(f"Using {num_threads} threads for parallel processing...\n")
+
+    print(f"""
+{Fore.GREEN}    .▄▄ · ▄• ▄▌▄▄▄▄·  ▄▄·  ▄ .▄▄ •▄ {Style.RESET_ALL}
+{Fore.GREEN}    ▐█ ▀. █▪██▌▐█ ▀█▪▐█ ▌▪██▪▐██▌▄▌▪{Style.RESET_ALL}
+{Fore.GREEN}    ▄▀▀▀█▄█▌▐█▌▐█▀▀█▄██ ▄▄██▀▐█▐▀▀▄·{Style.RESET_ALL}
+{Fore.RED}    ▐█▄▪▐█▐█▄█▌██▄▪▐█▐███▌██▌▐▀▐█.█▌{Style.RESET_ALL}
+{Fore.RED}     ▀▀▀▀  ▀▀▀ ·▀▀▀▀ ·▀▀▀ ▀▀▀ ··▀  ▀{Style.RESET_ALL}
+{Fore.RED}                                                                                
+{Style.RESET_ALL}
+                                               subchk ~ 𝙀𝙂𝘿𝙖𝙧𝙠𝙤  """)
+    print(f"{Fore.RED}!notice You should put the subdomains list in : subdomains.txt{Style.RESET_ALL}")
+    print("Tool started.... ")
+
+    # Open the output file to write results
+    with open("output_results.txt", "w") as output_file:
+        with open("subdomains.txt", "r") as subs:
+           
+            subdomains = [line.strip() for line in subs]
+            
+            with ThreadPoolExecutor(max_workers=num_threads) as executor: 
+                
+                executor.map(lambda url: check_subdomain(url, output_file), subdomains)
+
+    print("Scan Finished. Results saved to 'output_results.txt'")
+
+
+main()
